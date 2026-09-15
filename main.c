b11d51e78bcd4602b7be3fb924fc20d10107d713
Minu-972003 <linaraminuli09@gmail.com>
2026-09-15 10:54:27 +0530

    started smart hospital project
    Created the basic C program for the Smart Hospital system.

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("====================================\n");
+    printf("       SMART HOSPITAL SYSTEM\n");
+    printf("====================================\n");
+
+    printf("Hospital system started successfully.\n");
+
+    return 0;
+}
