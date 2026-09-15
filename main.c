3c9e17f3e2534930da0455ce5515e84c669d0823
Minu-972003 <linaraminuli09@gmail.com>
2026-09-15 12:14:42 +0530

    added doctor specialty data
    Added specialty names, fees, consultation times and daily patient limits.

--- main.c
+++ main.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+char spname[4][30] = {
+    "General Practice (OPD)",
+    "Paediatrics",
+    "Cardiology",
+    "Neurology"
+};
+
+float spfee[4] = {1500, 2500, 4500, 5000};
+
+int sptime[4] = {15, 20, 30, 30};
+
+int spcap[4] = {30, 20, 12, 10};
+
 int main()
 {
     printf("====================================\n");
