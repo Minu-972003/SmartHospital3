ac02c1d3ef96316c0700f0bbf0c60b0fa0515258
Minu-972003 <linaraminuli09@gmail.com>
2026-09-15 12:45:12 +0530

    added hospital ward data
    Added ward names, daily bed rates and bed capacities.

--- main.c
+++ main.c
@@ -13,6 +13,17 @@ int sptime[4] = {15, 20, 30, 30};
 
 int spcap[4] = {30, 20, 12, 10};
 
+char wname[4][30] = {
+    "General Ward",
+    "Paediatric Ward",
+    "Surgical Ward",
+    "ICU"
+};
+
+float wrate[4] = {3000, 6000, 12000, 25000};
+
+int wcap[4] = {20, 10, 10, 5};
+
 int main()
 {
     printf("====================================\n");
