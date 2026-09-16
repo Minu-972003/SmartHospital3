d628199f012cb166b01901ad7999e39c59803adb
Minu-972003 <linaraminuli09@gmail.com>
2026-09-16 12:17:01 +0530

    added patient data arrays
    Added parallel arrays for storing patient information.

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define MAX 100
 
 char spname[4][30] = {
     "General Practice (OPD)",
@@ -25,6 +26,16 @@ float wrate[4] = {3000, 6000, 12000, 25000};
 int wcap[4] = {20, 10, 10, 5};
 
 int bed[4][20] = {0};
+
+char pname[MAX][50];
+int age[MAX];
+int urgent[MAX];
+int spec[MAX];
+int admit[MAX];
+int ward[MAX];
+int days[MAX];
+
+int n = 0;
 
 int main()
 {
